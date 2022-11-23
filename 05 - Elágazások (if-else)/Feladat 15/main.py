5903a1cc8965592d815f9140113d02b9ad8ec11e
jamaikaivasutas <captainmine0@gmail.com>
2022-11-23 07:55:40 +0100

    20221123

--- 05 - Elágazások (if-else)/Feladat 15/main.py
+++ 05 - Elágazások (if-else)/Feladat 15/main.py
@@ -1,4 +1,4 @@
-from os import * 
+from os import *
 
 vegetableSoup: bool = False
 meatSoup: bool = False
@@ -19,6 +19,11 @@ cola: bool = False
 
 choice: int = None
 secondChoice: int = None
+thirdChoice: int = None
+
+isThereAppetizer: bool = False
+isThereMainCourse: bool = False
+isThereASideDish: bool = False
 
 print("Kérem a menüben szereplő ételeket/köreteket!")
 print("Zöldségleves [1]")
@@ -27,11 +32,11 @@ print("Gyümölcsleves [3]")
 
 choice = int(input())
 
-if choice == 1:
+if (choice == 1):
     vegetableSoup = True
-elif choice == 2:
+elif (choice == 2):
     meatSoup = True
-elif choice == 3:
+elif (choice == 3):
     fruitSoup = True
 
 print("Sültcsirkecomb [1]")
@@ -42,7 +47,42 @@ print("Pizza [5]")
 
 secondChoice = int(input())
 
+if (secondChoice == 1):
+    chickenThight = True
+elif (secondChoice == 2):
+    chickenBreast = True
+elif (secondChoice == 3):
+    vegetableGratin = True
+elif (secondChoice == 4):
+    spagetti = True
+elif (secondChoice == 5):
+    pizza = True
 
+print("Rizs [1]")
+print("Pároltzöldség [2]")
+print("Gyümölcs [3]")
+print("Sültkrumpli [4]")
+print("Saláta [5]")
+print("Kóla [6]")
 
+thirdChoice = int(input())
 
+if (thirdChoice == 1):
+    rice = True
+elif (thirdChoice == 2):
+    steamedVegetables = True
+elif (thirdChoice == 3):
+    fruit = True
+elif (thirdChoice == 4):
+    frenchFries = True
+elif (thirdChoice == 5):
+    salad = True
+elif (thirdChoice == 6):
+    cola = True
 
+isThereAppetizer = (vegetableSoup or meatSoup or fruitSoup)
+isThereMainCourse = (chickenThigh or chickenBreast or vegetableGratin or spagetti or pizza)
+isThereASideDish = (rice or steamedVegetables or fruit or frenchFries or salad or cola)
+    
+if (isThereAppetizer or isThereMainCourse or isThereASideDish):
+    if ()
