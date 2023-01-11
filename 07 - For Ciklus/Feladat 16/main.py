2c34608d812ab7be7740603108e29c329622c6f0
jamaikaivasutas <b1duzale@vasvari.org>
2023-01-11 07:54:24 +0100

    20230111

--- 07 - For Ciklus/Feladat 16/main.py
+++ 07 - For Ciklus/Feladat 16/main.py
@@ -3,6 +3,9 @@ from os import *
 startInterval : int = None
 endInterval : int = None
 count : int = 0
+countTwo: int = 0
+countThree: int = 0
+
 
 print("Kérem a kezdő intervallumot!")
 startInterval = int(input())
@@ -13,8 +16,15 @@ endInterval = int(input())
 system('cls')
 
 for i in range(startInterval, endInterval, 1):
-    if i % 3 == 0 and i % 2 != 0:
-        count = count + 1
+    if i 0:
+        countThree = countThree + 1
+    elif i % 2 == 0:
+        count = count + i
+    elif i % 2 != 0:
+        countTwo = countTwo + i
+   
 
-print(f"Összesen {count} szám van ami páratlan, és osztható hárommal!")
+average = (count + countTwo) / countThree
+print(average)
+
 
