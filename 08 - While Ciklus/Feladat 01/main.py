db021db4fb538e675687bdac6f7ac817439fd9fa
jamaikaivasutas <b1duzale@vasvari.org>
2023-01-25 07:49:37 +0100

    20230125

--- 08 - While Ciklus/Feladat 01/main.py
+++ 08 - While Ciklus/Feladat 01/main.py
@@ -1,13 +1,15 @@
 number: int = None
 temp: str = None
 isNumber: bool = False
+truncatedString: str = None
 
 while(number == None or (number < 0 or number > 9)):
     print("Adjon meg egy számot: ", end="")
     temp = input()
-    isNumber = isinstance(temp, (int, float ))
+    truncatedString = temp.replace(".","").replace("-","")
+    isNumber = truncatedString.isnumeric()
 
-    if (isNumber):
-        number : float(temp) 
+    if (isNumber) :
+        number = float(temp) 
     else:
         print("Nem számot adott meg!")
