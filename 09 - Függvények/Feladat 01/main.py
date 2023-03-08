c851fdea4298b4b14745082f6fc7fdcb1d07d818
jamaikaivasutas <b1duzale@vasvari.org>
2023-03-08 07:25:10 +0100

    2023marcius8

--- 09 - Függvények/Feladat 01/main.py
+++ 09 - Függvények/Feladat 01/main.py
@@ -4,13 +4,27 @@ from consoleIO import *
 
 x: float = None
 y: float = None
-result: float = None
+sumResult: float = None
+extractionResult: float = None
+multiplyResult: float = None
+divisionResult: float = None
 
 x = getNumberFromConsole()
 y = getNumberFromConsole()
 
-result = sumOfTwoNumbers(x,y)
+sumResult = sumOfTwoNumbers(x,y)
 
-printToConsole(x, y, result, "+")
+printToConsole(x, y, sumResult, "+")
 
+extractionResult = extractionOfTwoNumbers(x,y)
+
+printToConsole(x, y, extractionResult,  "-")
+
+multiplyResult = multiplicationOfTwoNumbers(x,y)
+
+printToConsole(x, y, multiplyResult, "*")
+
+divisionResult = divisionOfTwoNumbers(x,y)
+
+printToConsole(x, y, divisionResult, "/")
 
